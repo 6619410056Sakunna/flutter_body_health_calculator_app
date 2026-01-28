fb8f8c3a380902b20928967b4be2039e304d8cfd
s6619410056Sakunna <s6619410056@sau.ac.th>
2026-01-28 12:33:32 +0700

    commid 03 : เสร็จHome

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/adout_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmi_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmr_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUi extends StatefulWidget {
   const HomeUi({super.key});
@@ -8,8 +12,55 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
+//สร้างตัวแปรควบคุมCurrent Index
+  int currentIndexStatus = 1;
+
+  List<Widget> showBody = [
+    BmiUi(),
+    AboutUi(),
+    BmrUi(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+        title: Text(
+          'Body Health Calculator',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) => {
+          setState(() {
+            currentIndexStatus = value;
+          }),
+        },
+        currentIndex: currentIndexStatus,
+        selectedItemColor: const Color.fromARGB(255, 103, 133, 70),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_3,
+              ),
+              label: 'BMI'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'About'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                FontAwesomeIcons.personBooth,
+              ),
+              label: 'BMR'),
+        ],
+      ),
+      body: showBody[currentIndexStatus],
+    );
   }
 }
--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/Home_ui.dart';
 
 class SplashScreenUi extends StatefulWidget {
   const SplashScreenUi({super.key});
@@ -9,7 +10,54 @@ class SplashScreenUi extends StatefulWidget {
 
 class _SplashScreenUiState extends State<SplashScreenUi> {
   @override
+  void initState() {
+    // TODO: implement initState
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeUi(),
+        ),
+      );
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(136, 160, 209, 127),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/calculater.png',
+              width: MediaQuery.of(context).size.width * 0.50,
+              height: MediaQuery.of(context).size.width * 0.50,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 20.0,
+            ),
+            Text(
+              'Body Health Calculater',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
